36477eafb1f64253abfaa4d216f1130bf618cd38
2020WB86391 <2020wb86391@wilp.bits-pilani.ac.in>
2024-04-30 01:42:03 +0530

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
 
-    int number1, number2, sum;
+    int number1, number2, difference;
 
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;
+    sum = number1 - number2;
 
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d - %d = %d", number1, number2, difference);
     return 0;
 }
 //function added in branch1
@@ -18,5 +18,5 @@ int func_add(int a, int b)
         // i am in main branch
         // i am making this changes in
         // one more change
-        return a+b;
+        return a-b;
 }
